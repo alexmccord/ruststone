fb30338eb2d91293675558fa14595b442853e760
Alexander McCord <alex@mccord.dev>
2022-12-25 22:59:41 -0800

    Never mind, they did pass but that's due to lack of assertions.

--- ruststone/tests/world_constraint_solving_tests.rs
+++ ruststone/tests/world_constraint_solving_tests.rs
@@ -77,6 +77,39 @@ fn and_gate() {
 
     assert!(world.get(Vec3(0, 1, 0)).unwrap().redstate().is_on());
     assert!(world.get(Vec3(0, 1, 2)).unwrap().redstate().is_on());
+    assert!(world.get(Vec3(2, 2, 0)).unwrap().redstate().is_off());
+    assert!(world.get(Vec3(2, 2, 2)).unwrap().redstate().is_off());
+    assert!(world.get(Vec3(3, 1, 1)).unwrap().redstate().is_off());
+    assert!(world.get(Vec3(4, 1, 1)).unwrap().redstate().is_off());
+}
+
+#[test]
+fn and_gate_left_torch_off() {
+    let mut world = World::new();
+
+    world[Vec3(0, 0, 2)] = Voxel::stone().voxel();
+    world[Vec3(0, 1, 2)] = Voxel::torch().voxel();
+    world[Vec3(1, 0, 2)] = Voxel::stone().voxel();
+    world[Vec3(1, 1, 2)] = Voxel::dust().voxel();
+
+    world[Vec3(2, 1, 0)] = Voxel::stone().voxel();
+    world[Vec3(2, 2, 0)] = Voxel::torch().voxel();
+    world[Vec3(2, 1, 1)] = Voxel::stone().voxel();
+    world[Vec3(2, 2, 1)] = Voxel::dust().voxel();
+    world[Vec3(2, 1, 2)] = Voxel::stone().voxel();
+    world[Vec3(2, 2, 2)] = Voxel::torch().voxel();
+
+    world[Vec3(3, 1, 1)] = Voxel::torch().facing_east().voxel();
+    world[Vec3(4, 0, 1)] = Voxel::stone().voxel();
+    world[Vec3(4, 1, 1)] = Voxel::dust().voxel();
+
+    world.run();
+
+    assert!(world.get(Vec3(0, 1, 2)).unwrap().redstate().is_on());
+    assert!(world.get(Vec3(2, 2, 0)).unwrap().redstate().is_on());
+    assert!(world.get(Vec3(2, 2, 2)).unwrap().redstate().is_off());
+    assert!(world.get(Vec3(3, 1, 1)).unwrap().redstate().is_on());
+    assert!(world.get(Vec3(4, 1, 1)).unwrap().redstate().is_on());
 }
 
 #[test]
